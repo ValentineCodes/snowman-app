2134791f08cdac0e86b9aced7c5c0630a7d8cf0b
Valentine Orga <valentineorga@gmail.com>
2025-06-13 14:37:39 +0100

    feat: implement snowman minting

---
+++ packages/reactnative/src/components/Snowman.tsx
@@ -0,0 +1,99 @@
+import base64 from 'base-64';
+import { Contract, JsonRpcProvider } from 'ethers';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Card, Text } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import { useDeployedContractInfo, useNetwork } from '../hooks/eth-mobile';
+import { COLORS } from '../utils/constants';
+import { WINDOW_WIDTH } from '../utils/styles';
+
+type Props = { id: number; remove: () => void };
+interface Metadata {
+  name: string;
+  image: string;
+}
+
+export default function Snowman({ id, remove }: Props) {
+  const [metadata, setMetadata] = useState<Metadata>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const network = useNetwork();
+
+  const { data: snowmanContract, isLoading: isLoadingSnowmanContract } =
+    useDeployedContractInfo('Snowman');
+
+  const getDetails = async () => {
+    if (isLoadingSnowmanContract) return;
+
+    try {
+      setIsLoading(true);
+      const provider = new JsonRpcProvider(network.provider);
+
+      const snowman = new Contract(
+        snowmanContract?.address,
+        snowmanContract?.abi,
+        provider
+      );
+
+      const tokenURI: string = await snowman.tokenURI(id);
+      const metadata = JSON.parse(
+        base64.decode(tokenURI.replace('data:applicaton/json;base64,', ''))
+      );
+      const decodedMetadataImage = base64.decode(
+        metadata.image.replace('data:image/svg+xml;base64,', '')
+      );
+      metadata.image = decodedMetadataImage;
+
+      setMetadata(metadata);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDetails();
+  }, [isLoadingSnowmanContract]);
+
+  if (isLoading)
+    return (
+      <Card
+        style={{
+          width: WINDOW_WIDTH * 0.4,
+          height: WINDOW_WIDTH * 0.4,
+          backgroundColor: '#fafafa'
+        }}
+      >
+        {null}
+      </Card>
+    );
+  if (!metadata) return null;
+
+  return (
+    <View>
+      <SvgXml
+        xml={metadata.image}
+        width={WINDOW_WIDTH * 0.4}
+        height={WINDOW_WIDTH * 0.4}
+      />
+      <View style={styles.id}>
+        <Text variant="bodyLarge" style={{ fontWeight: '500' }}>
+          {metadata.name}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  id: {
+    position: 'absolute',
+    top: 7,
+    left: 2,
+    backgroundColor: COLORS.primaryLight,
+    paddingHorizontal: 8,
+    borderRadius: 4
+  }
+});
