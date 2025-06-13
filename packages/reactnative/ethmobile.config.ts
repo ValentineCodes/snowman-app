e8f0410fad844f9c3fd04e8dd7e5146c322b7bd1
Valentine Orga <valentineorga@gmail.com>
2025-06-13 14:17:09 +0100

    chore: deploy contracts

--- packages/reactnative/ethmobile.config.ts
+++ packages/reactnative/ethmobile.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
   networks: {
     localhost: {
       name: 'Localhost',
-      provider: 'http://192.168.0.124:8545',
+      provider: 'http://192.168.215.126:8545',
       id: 1337,
       currencySymbol: 'ETH',
       coingeckoPriceId: 'ethereum',
